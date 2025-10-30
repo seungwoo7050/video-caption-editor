04cf46f92a2b97c478c510966467fd49b9629c70
woopinbell <seungwoo7050@naver.com>
2025-10-30 16:09:51 +0900

    build(web): install react-query and set global query defaults

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,9 @@
+import { QueryClientProvider } from '@tanstack/react-query';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { RouterProvider } from 'react-router-dom';
 
+import { queryClient } from '@/lib/queryClient';
 import { router } from '@/router';
 import './index.css';
 
@@ -11,6 +13,8 @@ logAppEnvOnce();
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <QueryClientProvider client={queryClient}>
+      <RouterProvider router={router} />
+    </QueryClientProvider>
   </React.StrictMode>,
 )
